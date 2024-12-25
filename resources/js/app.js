50c758a307e2662994c1fa1d4d0726647cf4442c
devbjoy <developerbjoy@gmail.com>
2024-12-25 18:59:39 +0600

    commit first code

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import "./bootstrap";
+import "flowbite";
+import "../../vendor/masmerise/livewire-toaster/resources/js";
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
